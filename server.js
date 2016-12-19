c2431b936a9aaf361dc8c0ac5cd52aa4c5848640
wztan <wztan@hotmail.co.uk>
2016-12-19 22:47:49 +0000

    Add server features to output request header information

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
+"use strict"
+
 var express = require("express");
 var app = express();
+
+app.get("/header", function(req, res) {
+    console.log("ip:", req.get("X-Forwarded-For"));
+    console.log("Accept-Language:", req.get("Accept-Language"));
+    console.log("user-agent:", req.get("User-Agent"));
+    var OS = req.get("User-Agent").match(/\([a-zA-Z\d\s\.\;]+\)/i)[0];
+    var info = {
+        ipaddress: req.get("X-Forwarded-For"),
+        language: req.get("Accept-Language").substring(0, 5),
+        software: OS.substring(1, OS.length-1)
+    };
+    res.end(JSON.stringify(info));
+});
+
+app.listen(8080, function() {
+    console.log("Request header parser listening on port 8080");
+})
